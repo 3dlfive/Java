31595f115eb8cc53ab2c6f633ee57d30c64a7da6
DenysTry <denystry@gmail.com>
2023-09-07 00:34:53 -0400

    feat: creat basic functionality of hw game

--- src/homework3/WeekTasks.java
+++ src/homework3/WeekTasks.java
@@ -1,37 +1,80 @@
 package homework3;
 
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class WeekTasks {
 
-    static String[][] scedule = new String[][] {{"Sunday","Monday","Thursday","Wednesday","Thursday","Friday","Saturday","Sunday"},{"do home work","go to courses","watch a film","go to the jym","read a book","make work research","drink alcohol"}};
+    static String[][] scedule = new String[][] {{"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"},{"do home work","go to courses","watch a film","go to the jym","read a book","make work research","drink alcohol"}};
+    static boolean gamestatus = true;
     public static void main(String[] args) {
 
-        System.out.println(Arrays.deepToString(scedule[0]));
-        String userInput ;
-        askUser();
+//        String userAnswear = fistCaseUpper(askUser());
+//        int arrIndex = findIndexOfCell(userAnswear,scedule[0]);
+        StartGame();
 
 
+
+
+
+    }
+    static void StartGame(){
+        String userAnswear = fistCaseUpper(askUser());
+        int arrIndex = findIndexOfCell(userAnswear,scedule[0]);
+
+        switch (userAnswear){
+            case "Sunday", "Tuesday", "Friday", "Thursday", "Wednesday", "Monday","Saturday":
+                System.out.printf(">On %2$s task os %1$s \n",scedule[1][arrIndex],scedule[0][arrIndex]);
+                StartGame();
+                break;
+            case "Exit":
+                gamestatus = false;
+                break;
+        }
     }
     static String askUser(){
         Scanner sc = new Scanner(System.in);
-        String userInput;
+        String userInput = null;
         System.out.println("Please , input the day of the week.");
         do{
+            if (userInput == null){
+                userInput = sc.next();
+            } else {
+                System.out.printf("Sorry, I don't understand you, please try again.%s \n",userInput);
+                userInput = sc.next();
+            }
 
-           System.out.println("Enter the write day:");
-            userInput = sc.next();
-
-        }while(!checkIfInTasks(userInput, scedule[0]));
-        System.out.printf("You chose %s!",userInput);
+        }while(!(checkTwoStrings(userInput,"exit")||checkIfInTasks(userInput, scedule[0])));
+//        System.out.printf("You chose %s! \n",userInput);
         return userInput;
     }
+    static Boolean checkTwoStrings(String a,String b){
+        return a.equalsIgnoreCase(b);
+    }
     static Boolean checkIfInTasks(String input, String[] arr){
+        for (String string : arr){
+            if (string.equalsIgnoreCase(input)){
+                return true;
+            }
+        }
+        return false;
+
+//        Arrays.sort(arr);
+//        int res = Arrays.binarySearch(arr, input);
+//        boolean result = res >= 0 ? true : false;
+//        return result;
+    }
+    static int findIndexOfCell(String input, String[] arr){
         Arrays.sort(arr);
-        int res = Arrays.binarySearch(arr, input);
-        boolean result = res >= 0 ? true : false;
-        return result;
+        int res = Arrays.binarySearch(arr,input);
+        return res;
+    }
+    static String fistCaseUpper(String input){
+        String lowerCased = input.toLowerCase();
+        String[] lowedArrChars = lowerCased.split("");
+        lowedArrChars[0] = lowedArrChars[0].toUpperCase(Locale.ROOT);
+        return String.join("",lowedArrChars);
     }
 }
