31a859511aa44fd7e2dbdac402d1d8aef43bfafb
DenysTry <denystry@gmail.com>
2023-09-10 23:27:17 -0400

    feat: feat Human and Pet  consturctors

--- src/homework4/Human.java
+++ src/homework4/Human.java
@@ -1,18 +1,34 @@
 package homework4;
 
+import java.util.Arrays;
+
 public class Human {
     String name;
     String surname;
     int year;
     int iq; // 0-100
-    Pet pet; // Object Pet
-    Human mother;
-    Human father;
+    Pet pet ; // Object Pet
+    Human mother = new Human();
+    Human father = new Human();
     String[][] schedule;
     void greetPet(){
         System.out.printf("Привіт, %s \n",pet);
     }
     void describePet(){
-        System.out.printf("У мене є %s, їй %s років, він %s",pet.species,pet.age,pet.trickLevel>50?"дуже хитрий":"майже не хитрий");
+        System.out.printf("У мене є %s, їй %s років, він %s \n",pet.species,pet.age,pet.trickLevel>50?"дуже хитрий":"майже не хитрий");
+    }
+    public String toString(){
+        String description = String.format("Human{name='%s', surname='%s', year=%d, iq=%d, mother=%s %s, father=%s %s, pet=%s{nickname='%s', age=%d, trickLevel=%d, habits=[%s]}} \n",name,surname,year,iq,mother.name,mother.surname,father.name,father.surname,pet.species,pet.nickname,pet.age,pet.trickLevel, Arrays.toString(pet.habits));
+        System.out.printf(description);
+        return description;
+    }
+}
+class HumanTest{
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        human1.name="Denys";
+        human1.surname="Try";
+
+        human1.describePet();
     }
 }
--- src/homework4/Pet.java
+++ src/homework4/Pet.java
@@ -2,12 +2,27 @@ package homework4;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Arrays;
+
 public class Pet {
     String species;
     String nickname;
     int age;
     int trickLevel; //0-100
     String[] habits;
+    Pet(String species2,String nickname2){
+        this(species2,nickname2,0,0,new String[]{"play on street","sleep a lot"});
+    }
+    Pet(){
+        this("cat","Dryzhok",1,40,new String[]{"murchatb","catch mouses"});
+    }
+    Pet(String species1,String nickname1,int age2,int trickLevel2,String[] habbits2){
+        species=species1;
+        nickname=nickname1;
+        age=age2;
+        trickLevel=trickLevel2;
+        habits=habbits2;
+    }
     void eat(){
         System.out.println("Я ї'м!");
     }
@@ -17,12 +32,17 @@ public class Pet {
     void foul(){
         System.out.println("Потрібно добре замести сліди...");
     }
+    public String toString(){
+        String myReturnString = String.format("%s{nickname='%s', age=%d, trickLevel=%d, habits=[%s]",species,nickname,age,trickLevel, Arrays.toString(habits));
+        System.out.println(myReturnString);
+        return myReturnString;
+    }
 }
 class PetTest {
     public static void main(String[] args) {
-        Pet sharik = new Pet();
-        sharik.nickname = "Sharik";
-        sharik.respond();
+        Pet sharik = new Pet("dog","Sharick",5,45, new String[]{"eat","sleep"});
+        sharik.toString();
+
     }
 
 }
