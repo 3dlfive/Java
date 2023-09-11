5188b5668052b1a7a2dbf916768189858b9503db
3lfive <denystry@gmail.com>
2023-09-11 15:39:18 -0400

    feat: getters and setters in Human and Pet, correct methods

--- src/homework4/Human.java
+++ src/homework4/Human.java
@@ -3,14 +3,14 @@ package homework4;
 import java.util.Arrays;
 
 public class Human {
-    String name;
-    String surname;
-    int year;
-    int iq; // 0-100
-    Pet pet ; // Object Pet
-    Human mother;
-    Human father;
-    String[][] schedule;
+    private String name;
+    private String surname;
+    private int year;
+    private int iq; // 0-100
+    private Pet pet ; // Object Pet
+    private Human mother;
+    private Human father;
+    private String[][] schedule;
     Human(){
         this("UnknownName","UnknownSurname",1991,new Pet(),null,null,new String[][] {{"Sun","Mon","Tue","Wed","Thur","Fri","Sat"},{}});
     }
@@ -23,25 +23,89 @@ public class Human {
 
     }
     Human(String nameArg,String surnameArg,int yearArg,Pet petArg,Human motherArg,Human fatherArg,String[][] scheduleArg){
-        name=nameArg;
-        surname=surnameArg;
-        year=yearArg;
-        pet=petArg;
-        mother=motherArg;
-        father=fatherArg;
-        schedule=scheduleArg;
-        System.out.println("New object Human was created %s\n");
+        this.setName(nameArg);
+        this.setSurname(surnameArg);
+        this.setYear(yearArg);
+        this.setPet(petArg);
+        this.setMother(motherArg);
+        this.setFather(fatherArg);
+        this.setSchedule(scheduleArg);
+        System.out.printf("New object Human was created %s %s \n",nameArg,surnameArg);
     }
     void greetPet(){
         System.out.printf("Привіт, %s \n",pet);
     }
     void describePet(){
-        System.out.printf("У мене є %s, їй %s років, він %s \n",pet.species,pet.age,pet.trickLevel>50?"дуже хитрий":"майже не хитрий");
+        System.out.printf("У мене є %s, їй %s років, він %s \n",pet.getSpecies(),pet.getAge(),pet.getTrickLevel()>50?"дуже хитрий":"майже не хитрий");
     }
     public String toString(){
-        String description = String.format("Human{name='%s', surname='%s', year=%d, iq=%d, mother=%s %s, father=%s %s, pet=%s{nickname='%s', age=%d, trickLevel=%d, habits=%s}} \n",name,surname,year,iq,mother.name,mother.surname,father.name,father.surname,pet.species,pet.nickname,pet.age,pet.trickLevel, Arrays.toString(pet.habits));
+        String description = String.format("Human{name='%s', surname='%s', year=%d, iq=%d, mother=%s %s, father=%s %s, pet=%s{nickname='%s', age=%d, trickLevel=%d, habits=%s}} \n",getName(),getSurname(),getYear(),getIq(),getMother().getName(),getMother().getSurname(),getFather().getName(),getFather().getSurname(),pet.getSpecies(),pet.getNickname(),pet.getAge(),pet.getTrickLevel(), Arrays.toString(pet.getHabits()));
         System.out.printf(description);
         return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public String[][] getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String[][] schedule) {
+        this.schedule = schedule;
     }
 }
 class HumanTest{
--- src/homework4/Pet.java
+++ src/homework4/Pet.java
@@ -1,27 +1,27 @@
 package homework4;
 
-import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
 
 public class Pet {
-    String species;
-    String nickname;
-    int age;
-    int trickLevel; //0-100
-    String[] habits;
+    private String species;
+    private String nickname;
+    private int age;
+    private int trickLevel; //0-100
+    private String[] habits;
     Pet(String species2,String nickname2){
         this(species2,nickname2,0,0,new String[]{"play on street","sleep a lot"});
     }
     Pet(){
         this("cat","Dryzhok",1,40,new String[]{"murchatb","catch mouses"});
     }
-    Pet(String species1,String nickname1,int age2,int trickLevel2,String[] habbits2){
-        species=species1;
-        nickname=nickname1;
-        age=age2;
-        trickLevel=trickLevel2;
-        habits=habbits2;
+    Pet(String species,String nickname,int age2,int trickLevel2,String[] habbits2){
+        this.setSpecies(species);
+        this.setNickname(nickname);
+        this.setAge(age2);
+        this.setTrickLevel(trickLevel2);
+        this.setHabits(habbits2);
+
     }
     void eat(){
         System.out.println("Я ї'м!");
@@ -37,6 +37,47 @@ public class Pet {
         System.out.println(myReturnString);
         return myReturnString;
     }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
 }
 class PetTest {
     public static void main(String[] args) {
