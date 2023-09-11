0e8b2c3352064a19e1ac8d2c0f535d8a60ce684c
DenysTry <denystry@gmail.com>
2023-09-10 22:45:51 -0400

    feat: creat fields for human class and make some methods for it

--- src/homework4/Human.java
+++ src/homework4/Human.java
@@ -1,4 +1,18 @@
 package homework4;
 
 public class Human {
+    String name;
+    String surname;
+    int year;
+    int iq; // 0-100
+    Pet pet; // Object Pet
+    Human mother;
+    Human father;
+    String[][] schedule;
+    void greetPet(){
+        System.out.printf("Привіт, %s \n",pet);
+    }
+    void describePet(){
+        System.out.printf("У мене є %s, їй %s років, він %s",pet.species,pet.age,pet.trickLevel>50?"дуже хитрий":"майже не хитрий");
+    }
 }
