6f2921579af9d6b610188c7722861e93bd52dd7f
3lfive <denystry@gmail.com>
2023-09-12 11:07:51 -0400

    feat: feat builder for class Family with condition atleast two members.

--- src/homework4/Family.java
+++ src/homework4/Family.java
@@ -8,45 +8,76 @@ public class Family {
     private Human[] children;
     private Pet pet;
 
+    protected Family(Human mother,Human father,Human[] children,Pet pet){
+        this.mother=mother;
+        this.father=father;
+        this.children=children;
+        this.pet=pet;
+    }
     public Human getMother() {
         return mother;
-    }
-
-    public void setMother(Human mother) {
-        this.mother = mother;
     }
 
     public Human getFather() {
         return father;
     }
 
-    public void setFather(Human father) {
-        this.father = father;
-    }
-
     public Human[] getChildren() {
         return children;
-    }
-
-    public void setChildren(Human[] children) {
-        this.children = children;
     }
 
     public Pet getPet() {
         return pet;
     }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
+    public static FamilyBuilder builder(){
+        return new FamilyBuilder();
     }
 
     @Override
     public String toString() {
         return "Family{" +
-                "mother=" + getMother() +
-                ", father=" + getFather() +
-                ", children=" + Arrays.toString(getChildren()) +
-                ", pet=" + getPet() +
+                "mother=" + mother +
+                ", father=" + father +
+                ", children=" + Arrays.toString(children) +
+                ", pet=" + pet +
                 '}';
+    }
+
+    public static class FamilyBuilder {
+        private Human mother;
+        private Human father;
+        private Human[] children;
+        private Pet pet;
+        public FamilyBuilder withMother(Human mother){
+            this.mother=mother;
+            return this;
+        }
+        public FamilyBuilder withFather(Human father){
+            this.father=father;
+            return this;
+        }
+        public FamilyBuilder withChildren(Human[] children){
+            this.children=children;
+            return this;
+        }
+        public FamilyBuilder withPet(Pet pet){
+            this.pet=pet;
+            return this;
+        }
+        public Family build(){
+            if (mother==null || father==null){
+                throw new IllegalStateException("We need mother and father ! ");
+            } else {
+                return new Family(mother,father,children,pet);
+            }
+
+        }
+    }
+}
+class FamilyTest{
+    public static void main(String[] args) {
+        Family f1 = Family.builder().withMother(new Human("Oksana","Pertovna",1991)).withFather(new Human("Oleg","Kopchik",1992))
+                .build();
+        System.out.println(f1.toString());
     }
 }
