babfc45283197566ab2522d8309c06c11dc43b70
3lfive <denystry@gmail.com>
2023-09-20 17:56:49 -0400

    test: add more test for count function in class Family

--- src/test/java/homework4/FamilyTest.java
+++ src/test/java/homework4/FamilyTest.java
@@ -1,17 +1,29 @@
 package homework4;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FamilyTest {
-    Family f1 = Family.builder().withMother(new Human("Oksana","Pertovna",1991)).withFather(new Human("Oleg","Kopchik",1992))
-            .build();
+    Family f1 ;
+    @BeforeEach
+    void setup(){
+        f1 = Family.builder().withMother(new Human("Oksana","Pertovna",1991)).withFather(new Human("Oleg","Kopchik",1992))
+                .build();
+    }
+    @AfterEach
+    void cleanup(){
+        f1 = null;
+    }
     @Test
-    void countFamily() {
+    @DisplayName(value = "User Family counter more than two.")
+    void iscount_Family_More_than_Two() {
 
         int resultCounting = f1.countFamily();
-        assertEquals(2,resultCounting);
+        assertTrue(resultCounting>=2);
+//        assertTrue(resultCounting<2);
+
+        /* assertEquals(2,resultCounting); */
     }
 
     @Test
